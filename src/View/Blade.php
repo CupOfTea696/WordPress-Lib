81ba852ec6ad5e1fbef646d49536f1978167c264
CupOfTea696 <cupoftea696@gmail.com>
2018-01-15 14:34:52 +0000

    [Blade] Fix $loop variable for @acfloop

--- src/View/Blade.php
+++ src/View/Blade.php
@@ -430,14 +430,14 @@ class Blade extends Service
                     $expression = $parent['expression'];
                 }
                 
-                $initLoop = '$loop = new ' . Counter::class . "(); \$loop->start(get_sub_field({$expression}) || get_field({$expression})); \$__currentLoopData = \$loop; \$__loops->addLoop(\$__currentLoopData);";
+                $initLoop = '$loop = new ' . Counter::class . "(); \$loop->start(get_sub_field({$expression}) ?: get_field({$expression})); \$__currentLoopData = \$loop; \$__loops->addLoop(\$__currentLoopData);";
                 
                 return "<?php {$initLoop} while(have_rows({$expression})): the_row(); ?>";
             }
         }
         
         $id = $this->openStack('acfloop', ['open' => true]);
-        $initLoop = '$loop = new ' . Counter::class . "(); \$loop->start(get_sub_field({$expression}) || get_field({$expression})); \$__currentLoopData = \$loop; \$__loops->addLoop(\$__currentLoopData);";
+        $initLoop = '$loop = new ' . Counter::class . "(); \$loop->start(get_sub_field({$expression}) ?: get_field({$expression})); \$__currentLoopData = \$loop; \$__loops->addLoop(\$__currentLoopData);";
         
         return "<?php if (have_rows({$expression})): {$initLoop} while(have_rows({$expression})): the_row(); ?>";
     }
