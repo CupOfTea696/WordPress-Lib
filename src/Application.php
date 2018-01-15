fdf618a3c119abb46a088822aab4623dd6c2cc27
CupOfTea696 <cupoftea696@gmail.com>
2018-01-15 14:58:16 +0000

    Release: v1.5.11

--- src/Application.php
+++ src/Application.php
@@ -44,7 +44,7 @@ class Application extends Container implements PackageContract
      *
      * @const string
      */
-    const VERSION = '1.5.10';
+    const VERSION = '1.5.11';
     
     /**
      * The base path for the WordPress installation.
