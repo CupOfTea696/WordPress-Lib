7b31413dff6f4f70b3e2a2fc8172d0721152891d
Sven Wittevrongel <cupoftea696@gmail.com>
2018-06-01 13:59:19 +0100

    Release: v1.5.12

--- src/Application.php
+++ src/Application.php
@@ -44,7 +44,7 @@ class Application extends Container implements PackageContract
      *
      * @const string
      */
-    const VERSION = '1.5.11';
+    const VERSION = '1.5.12';
     
     /**
      * The base path for the WordPress installation.
